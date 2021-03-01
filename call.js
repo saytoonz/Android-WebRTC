14b474698368d2944e1378026d8c4709500b23bf
saytoonz <rassay31@gmail.com>
2021-03-01 02:53:05 +0000

    First commit

--- call.js
+++ call.js
@@ -13,7 +13,15 @@ remoteVideo.onplaying = () => {remoteVideo.style.opacity = 1}
 let peer
 
 const init = userId =>{
-    peer = new Peer(userId)
+    peer = new Peer(userId, {
+        host: '192.168.0.162',
+        port: 9000,
+    })
+
+    peer.on('open', () => {
+        //WE WILL MAKE A CALL TO A KOTLIN/JAVA/FLUTTER FUNCTION
+    })
+
     listen()
 }
 
@@ -36,4 +44,42 @@ const listen =()=>{
             })
         })
     })
+}
+
+
+
+const startCall = (otherUserId) => {
+    navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: true
+    }).then(stream => {
+        localVideo.srcObject = stream
+        localStream = stream
+
+        const call = peer.call(otherUserId, stream)
+        call.on('stream', remoteStream => {
+            remoteVideo.srcObject = remoteStream
+
+            remoteVideo.className = "primary-video"
+            localVideo.className = "secondary-video"
+        })
+    })
+}
+
+
+
+const toggleAudio = (stringBool) => {
+    if(stringBool == "true"){
+        localStream.getAudioTracks()[0].enabled = true
+    }else{
+        localStream.getAudioTracks()[0].enabled = false
+    }
+}
+
+const toggleVideo = (stringBool) => {
+    if(stringBool == "true"){
+        localStream.getVideoTracks()[0].enabled = true
+    }else{
+        localStream.getVideoTracks()[0].enabled = false
+    }
 }
